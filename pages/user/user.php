82fe356ee99cdf93d60977c2c036badce1f57c65
nguyenquocdat <boelovenhan@gmail.com>
2017-11-14 23:13:34 -0500

    add file pages/user user.php danhsachcungnhom.php thongtincanhan.php

---
+++ pages/user/user.php
@@ -0,0 +1,14 @@
+<li>
+  <a href="./thongtincanhan.php">
+    <i class="fa fa-th"></i> <span>Thông tin cá nhân</span>
+    <span class="pull-right-container">
+    </span>
+  </a>
+</li>
+<li>
+  <a href="./danhsachcungnhom.php">
+    <i class="fa fa-group"></i> <span>Danh sách cùng nhóm</span>
+    <span class="pull-right-container">
+    </span>
+  </a>
+</li>
